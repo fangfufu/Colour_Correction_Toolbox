da106568065093916cf8562c95159d557e10b22e
Fufu Fang <fangfufu@smithsonian.fangfufu.co.uk>
2017-07-27 10:21:02 +0100

    removed bin/EvalCCCSSFCMF.m

--- bin/EvaluateAll.m
+++ bin/EvaluateAll.m
@@ -21,45 +21,45 @@ foldInd = crossvalind('KFold',size(XYZ,1), 3);
 t = table();
 i = 1;
 
-% [ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, @GenCCLinear, @ApplyCCLinear, foldInd);
-% t(i,:) = GenerateTableRow(cielabE, 'Linear least squared');
-% i = i + 1;
-% 
-% GenCCPoly2 = @(rgb, XYZ) GenCCPolynomial(rgb, XYZ, 2, 0);
-% [ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, GenCCPoly2, @ApplyCCPolynomial, foldInd);
-% t(i,:) = GenerateTableRow(cielabE, 'Second order polynomial');
-% i = i + 1;
-% 
-% GenCCPoly3 = @(rgb, XYZ) GenCCPolynomial(rgb, XYZ, 3);
-% [ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, GenCCPoly3, @ApplyCCPolynomial, foldInd);
-% t(i,:) = GenerateTableRow(cielabE, 'Third order polynomial');
-% i = i + 1;
-% 
-% GenCCRP2 = @(rgb, XYZ) GenCCRootPolynomial(rgb, XYZ, 2, 0);
-% [ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, GenCCRP2, @ApplyCCRootPolynomial, foldInd);
-% t(i,:) = GenerateTableRow(cielabE, 'Second order root-polynomial');
-% i = i + 1;
-% 
-% GenCCRP3 = @(rgb, XYZ) GenCCRootPolynomial(rgb, XYZ, 3);
-% [ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, GenCCRP3, @ApplyCCRootPolynomial, foldInd);
-% t(i,:) = GenerateTableRow(cielabE, 'Third order root-polynomial');
-% i = i + 1;
-% 
-% [ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, @GenCCHomo, @ApplyCCLinear, foldInd);
-% t(i,:) = GenerateTableRow(cielabE, '2D homography');
-% i = i + 1;
-% 
-% [ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, @GenCCEnsemble, @ApplyCCEnsemble, foldInd);
-% t(i,:) = GenerateTableRow(cielabE, 'Ensemble method');
-% i = i + 1;
-% 
-% [ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, @GenCCHPP, @ApplyCCHPP, foldInd);
-% t(i,:) = GenerateTableRow(cielabE, 'Hue Plane Preserving');
-% i = i + 1;
-% 
-% [ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, @GenCCAM, @ApplyCCLinear, foldInd);
-% t(i,:) = GenerateTableRow(cielabE, 'Angular Minimisation');
-% i = i + 1;
+[ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, @GenCCLinear, @ApplyCCLinear, foldInd);
+t(i,:) = GenerateTableRow(cielabE, 'Linear least squared');
+i = i + 1;
+
+GenCCPoly2 = @(rgb, XYZ) GenCCPolynomial(rgb, XYZ, 2, 0);
+[ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, GenCCPoly2, @ApplyCCPolynomial, foldInd);
+t(i,:) = GenerateTableRow(cielabE, 'Second order polynomial');
+i = i + 1;
+
+GenCCPoly3 = @(rgb, XYZ) GenCCPolynomial(rgb, XYZ, 3);
+[ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, GenCCPoly3, @ApplyCCPolynomial, foldInd);
+t(i,:) = GenerateTableRow(cielabE, 'Third order polynomial');
+i = i + 1;
+
+GenCCRP2 = @(rgb, XYZ) GenCCRootPolynomial(rgb, XYZ, 2, 0);
+[ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, GenCCRP2, @ApplyCCRootPolynomial, foldInd);
+t(i,:) = GenerateTableRow(cielabE, 'Second order root-polynomial');
+i = i + 1;
+
+GenCCRP3 = @(rgb, XYZ) GenCCRootPolynomial(rgb, XYZ, 3);
+[ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, GenCCRP3, @ApplyCCRootPolynomial, foldInd);
+t(i,:) = GenerateTableRow(cielabE, 'Third order root-polynomial');
+i = i + 1;
+
+[ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, @GenCCHomo, @ApplyCCLinear, foldInd);
+t(i,:) = GenerateTableRow(cielabE, '2D homography');
+i = i + 1;
+
+[ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, @GenCCEnsemble, @ApplyCCEnsemble, foldInd);
+t(i,:) = GenerateTableRow(cielabE, 'Ensemble method');
+i = i + 1;
+
+[ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, @GenCCHPP, @ApplyCCHPP, foldInd);
+t(i,:) = GenerateTableRow(cielabE, 'Hue Plane Preserving');
+i = i + 1;
+
+[ cielabE ] = EvalCCRGBXYZ(RGB, XYZ, @GenCCAM, @ApplyCCLinear, foldInd);
+t(i,:) = GenerateTableRow(cielabE, 'Angular Minimisation');
+i = i + 1;
 
 %% Change data set for maximum ignorance colour correction
 load('HanGong_2015_Outdoor_Cloudy_RGB.mat');
